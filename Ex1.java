a1fe98153436692050c6117e6e0118d7ea45d798
Humberto Sobrinho <humberto.alves.sobrinho@gmail.com>
2016-03-10 17:43:29 -0300

    adicionando comentarios

--- Ex1.java
+++ Ex1.java
@@ -6,12 +6,15 @@ class Ex1{
 		double Marco;
 		double BalancoTrimestral;
 
+		//Atribuição de valores
 		Janeiro = 15000;
 		Fevereiro = 23000;
 		Marco = 17000;
 
+		//Processamento
 		BalancoTrimestral = Janeiro + Fevereiro + Marco;
 
+		//Saída de dados
 		System.out.println("Balanco trimestral.: "+BalancoTrimestral);
 	}
 }
