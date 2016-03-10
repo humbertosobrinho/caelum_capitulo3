a62ced3c6f5276a119a7807edd0d1addc8e5de77
Humberto Sobrinho <humberto.alves.sobrinho@gmail.com>
2016-03-10 17:40:08 -0300

    Exercicio 1 capitulo 3

---
+++ Ex1.java
@@ -0,0 +1,17 @@
+class Ex1{
+	public static void main(String args[]){
+		//declaração de variaveis
+		double Janeiro;
+		double Fevereiro;
+		double Marco;
+		double BalancoTrimestral;
+
+		Janeiro = 15000;
+		Fevereiro = 23000;
+		Marco = 17000;
+
+		BalancoTrimestral = Janeiro + Fevereiro + Marco;
+
+		System.out.println("Balanco trimestral.: "+BalancoTrimestral);
+	}
+}
